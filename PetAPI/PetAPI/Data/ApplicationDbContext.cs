80d727ef3c1962c1c8f0a644400847e04c5a87b2
YasarKabaksiz <yasarka82@gmail.com>
2025-06-21 15:46:35 +0300

    feat: Initial project setup with EF Core and User entity.

---
+++ PetAPI/PetAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using PetAPI.Entities;
+
+namespace PetAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // User entity konfigürasyonu
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.PasswordHash).IsRequired();
+                entity.Property(e => e.Role).IsRequired().HasMaxLength(20).HasDefaultValue("User");
+                
+                // Username benzersiz olmalı
+                entity.HasIndex(e => e.Username).IsUnique();
+            });
+        }
+    }
+} 
---
+++ PetAPI/PetAPI/Entities/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PetAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; } = string.Empty;
+        
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(20)]
+        public string Role { get; set; } = "User";
+    }
+} 
